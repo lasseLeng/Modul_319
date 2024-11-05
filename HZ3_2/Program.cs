38c85eef324bd3653b535b1835285a3c7af9293d
Lasse Noel Lengacher <L.Lengacher.inf24@stud.bbbaden.ch>
2024-11-05 14:30:48 +0100

    Code HZ3_2 erweitert

--- HZ3_2/Program.cs
+++ HZ3_2/Program.cs
@@ -11,23 +11,23 @@ class Program
         double lbNote = Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Gib die OBA-Punkte für Aufgabenkontrolle 1 ein: ");
-        double obaPunkteAK1 = Convert.ToDouble(Console.ReadLine());
+        double obaPunkte1 = Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Gib die OBA-Punkte für Aufgabenkontrolle 2 ein: ");
-        double obaPunkteAK2 = Convert.ToDouble(Console.ReadLine());
+        double obaPunkte2 = Convert.ToDouble(Console.ReadLine());
 
         Console.Write("Gib die OBA-Punkte für Portfolioartikel ein: ");
         double obaPunktePortfolio = Convert.ToDouble(Console.ReadLine());
 
-        double obaNote = (obaPunkteAK1 + obaPunkteAK2 + obaPunktePortfolio) / 3.0;
+        double obaNote = (obaPunkte1 + obaPunkte2 + obaPunktePortfolio) / 3.0;
 
         double modulNote = (lbNote * 0.8) + (obaNote * 0.2);
 
         Console.WriteLine($"Du hast im Modul M{modulNummer} die Note {modulNote:F2} erreicht. Diese setzt sich folgendermassen zusammen:");
         Console.WriteLine($"LB-Note (80%): {lbNote}");
         Console.WriteLine($"OBA (20%): {obaNote:F2}");
-        Console.WriteLine($"Aufgabenkontrolle 1: {obaPunkteAK1}");
-        Console.WriteLine($"\tAufgabenkontrolle 2: {obaPunkteAK2}");
+        Console.WriteLine($"Aufgabenkontrolle 1: {obaPunkte1}");
+        Console.WriteLine($"\tAufgabenkontrolle 2: {obaPunkte2}");
         Console.WriteLine($"\tPortfolio-Artikel: {obaPunktePortfolio}");
 
         Console.ReadKey();
