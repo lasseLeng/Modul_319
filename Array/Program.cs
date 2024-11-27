74c481dcfa9877e5b5164092c7c74fb3a57b4e9c
Lasse Noel Lengacher <L.Lengacher.inf24@stud.bbbaden.ch>
2024-11-27 19:26:38 +0100

    Array anpassung

--- Array/Program.cs
+++ Array/Program.cs
@@ -4,23 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Definiere die Größe des Arrays
-        int[] zahlen = new int[5]; // Array mit Platz für 5 Zahlen
+        int[] noten = new int[5]; 
 
-        Console.WriteLine("Bitte geben Sie 5 Zahlen ein:");
+        Console.WriteLine("Bitte geben Sie 5 Noten ein:");
 
-        // Eingabe der Zahlen
-        for (int i = 0; i < zahlen.Length; i++)
+        for (int i = 0; i < noten.Length; i++)
         {
-            Console.Write("Zahl " + (i + 1) + ": ");
-            zahlen[i] = int.Parse(Console.ReadLine());
+            Console.Write("Note " + (i + 1) + ": ");
+            noten[i] = int.Parse(Console.ReadLine());
         }
 
-        // Ausgabe der Zahlen
-        Console.WriteLine("\nDie eingegebenen Zahlen sind:");
-        for (int i = 0; i < zahlen.Length; i++)
+        Console.WriteLine("\nDie eingegebenen Noten sind:");
+        for (int i = 0; i < noten.Length; i++)
         {
-            Console.WriteLine("Zahl " + (i + 1) + ": " + zahlen[i]);
+            Console.WriteLine("Zahl " + (i + 1) + ": " + noten[i]);
         }
+        Console.ReadKey();
     }
 }
